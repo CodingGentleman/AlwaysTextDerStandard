12c492bc63aa33fb6266bbc3e9684e6333890e02
CodingGentleman <schmid.kevin.manuel@gmail.com>
2018-10-08 13:22:21 +0200

    set redirect to https, changed namespace

--- alwaystextderstandard.js
+++ alwaystextderstandard.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name         Always text.derstandard.at
-// @namespace    http://tampermonkey.net/
-// @version      0.1
+// @namespace    https://www.codinggentleman.com
+// @version      0.2
 // @description  Redirect *.derstandard to text.derstandard
-// @include      *://*derstandard.at/*
+// @include      http*://*derstandard.at/*
 // @grant        none
-// @run-at        document-start
+// @run-at       document-start
 // ==/UserScript==
 
 (function() {
     'use strict';
     if(location.href.indexOf("text.derstandard.at") === -1) {
         var index = location.href.indexOf("derstandard.at");
-        location.replace("http://text." + location.href.substring(index));
+        location.replace("https://text." + location.href.substring(index));
     }
 })();
